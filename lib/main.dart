3618d3d9fed2815bd0807807dbed59be25ab1f43
Jeenit <sharmajeenit2000@gmail.com>
2023-01-13 01:06:37 +0530

    Stateful widget(dynamic question text)

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/question.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  static const List<String> questions = [
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  //underscore makes the property/Class private cannot be accessed anywhere else other than this file
+  static const List<String> _questions = [
     "What's your favorite color?",
     "What's your favorite animal?",
   ];
-  int questionIndex = 0;
-
-  MyApp({super.key});
-
-  void answerQuestion() {
-    questionIndex++;
-    print(questionIndex);
+  int _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = (_questionIndex + 1) % _questions.length;
+    });
   }
 
   @override
@@ -26,21 +35,21 @@ class MyApp extends StatelessWidget {
           title: const Text('Quiz'),
         ),
         body: Column(children: [
-          Text(questions[questionIndex]),
+          Question(question: _questions[_questionIndex]),
           ElevatedButton(
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
             child: const Text('Answer 1'),
           ),
           ElevatedButton(
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
             child: const Text('Answer 2'),
           ),
           ElevatedButton(
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
             child: const Text('Answer 3'),
           ),
           ElevatedButton(
-            onPressed: answerQuestion,
+            onPressed: _answerQuestion,
             child: const Text('Answer 4'),
           ),
         ]),
---
+++ lib/question.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/Material.dart';
+
+class Question extends StatelessWidget {
+  final String question;
+  const Question({super.key, required this.question});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(question);
+  }
+}
